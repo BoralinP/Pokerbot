7b7fbe949c6e89d22f7196c0c792942da913883e
tp <tp@DESKTOP-CIJ9F7V>
2018-10-20 04:17:16 +0300

    Uusin versio

--- Poker-bot/src/poker/bot/Logiikka/kuvanTunnistus.java
+++ Poker-bot/src/poker/bot/Logiikka/kuvanTunnistus.java
@@ -5,46 +5,12 @@
  */
 package poker.bot.Logiikka;
 
-import java.awt.AWTException;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.image.BufferedImage;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+//import static poker.bot.Application.ApplicationUI.näytäKuva;
 
 /**
  *
  * @author tporanen
  */
 public class kuvanTunnistus {
-
-    public Robot s; //Robotti (ottaa kuvan)
-    public boolean paalla = false;
-    public BufferedImage screenShot;
-
-    public kuvanTunnistus() {
-        paalla = true;
-        try {
-            s = new Robot(); //Luodaan robotti nimeltä s.
-        } catch (AWTException ex) {
-            //Logger.getLogger(kuvanTunnistus.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Botti päällä! - " + getAika());
-    }
-
-    public void uusiKuvankaappaus() {
-        System.out.println(getAika() + " - ");
-        screenShot = s.createScreenCapture(new Rectangle(2, 2, 2, 2)); //public Rectangle(int x, int y, int width, int height) {
-        /*
-            Tutkitaan mikä kirjain on kuvassa
-            OpenCV kuvantunnistus...
-         */
-
-    }
-
-    public String getAika() {
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        return sdf.format(cal.getTime());
-    }
+    
 }
